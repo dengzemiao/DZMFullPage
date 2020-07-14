647f78e6596865750263d950adb900d5f1979cac
dengzemiao <dengzemiaovip@163.com>
2020-07-14 17:19:07 +0800

    优化回调数据

--- fullPage.js
+++ fullPage.js
@@ -15,6 +15,10 @@ function fullPage (target, params) {
   // 初始化调用
   onScroll(childrens, childrenRects, params)
 }
+// 可见子元素数组索引
+var visibleIndexs = []
+// 可见子元素数组索引记录
+var visibleIndexsOld = []
 /**
  * 滚动出来函数
  * @param {*} childrenRects 子元素范围值
@@ -34,8 +38,6 @@ function onScroll (childrens, childrenRects, params) {
   var scrollHeight = document.documentElement.clientHeight || document.body.clientHeight
   // 窗口滚动范围最大Y值
   var scrollMaxY = scrollY + scrollHeight
-  // 可见子元素数组
-  var childrenVisibles = []
   // 便利子元素范围
   childrenRects.forEach(function (rect, index) {
     // 显示多少比例算可见
@@ -46,13 +48,38 @@ function onScroll (childrens, childrenRects, params) {
     var rectMaxY = rect.maxY - (visibleFooterScale > 1 ? visibleHeaderScale : rect.height * visibleFooterScale)
     // 判断是否在显示范围内
     var isVisible = (rectY >= scrollY && rectY <= scrollMaxY) || (rectMaxY >= scrollY && rectMaxY <= scrollMaxY) || (scrollY >= rectY && scrollMaxY <= rectMaxY) || (scrollMaxY >= rectY && scrollMaxY <= rectMaxY)
-    // 可见子元素
-    if (isVisible) { childrenVisibles.push(index) }
-    // 普通回调
-    if (callback) { callback(index, isVisible, childrens) }
+    // 获得在可见子元素列表索引
+    var indx = visibleIndexs.indexOf(index)
+    // 当前子元素可见状态
+    if (isVisible) { 
+      // 是否存在，不存在则添加
+      if (indx === -1) {
+        // 添加
+        visibleIndexs.push(index)
+        // 回调
+        if (callback) { callback(index, isVisible, visibleIndexs, childrens) }
+      }
+    } else {
+      // 是否存在，存在则移除
+      if (indx !== -1) {
+        // 移除
+        visibleIndexs.splice(indx, 1)
+        // 回调
+        if (callback) { 
+          callback(index, isVisible, visibleIndexs, childrens) 
+        }
+      }
+    }
   })
   // 可见子元素回调
-  if (callbackVisibles) { callbackVisibles(childrenVisibles, childrens) }
+  if (callbackVisibles) {
+    // 是否为重复数据
+    var isRepeat = visibleIndexsOld.length ? visibleIndexsOld.join() === visibleIndexs.join() : false
+    // 如果没有重复数据则回调出去
+    if (!isRepeat) { callbackVisibles(visibleIndexs, childrens) }
+    // 记录数据下次用于比较
+    visibleIndexsOld = visibleIndexs.concat()
+  }
 }
 /**
  * 获取所有子元素当前状态的范围值
