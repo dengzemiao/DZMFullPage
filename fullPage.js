f8e04f88c77af4fd74aa99fa002f5d010a0acdbd
dengzemiao <dengzemiaovip@163.com>
2020-07-14 15:47:21 +0800

    .

--- fullPage.js
+++ fullPage.js
@@ -25,7 +25,9 @@ function onScroll (childrens, childrenRects, params) {
   // 返回可见子元素列表
   var callbackVisibles = (params && params.callbackVisibles) ? params.callbackVisibles : null
   // 获取每个子元素头部出现多少可见区域才算可见（小于1: 按子元素本身高度的百分比计算px, 大于1: 按px进行计算）
-  var callbackVisibleScales = (params && params.callbackVisibleScales) ? params.callbackVisibleScales : function () { return 0 }
+  var callbackVisibleHeaderScales = (params && params.callbackVisibleHeaderScales) ? params.callbackVisibleHeaderScales : function () { return 0 }
+  // 获取每个子元素尾部出现多少可见区域才算可见（小于1: 按子元素本身高度的百分比计算px, 大于1: 按px进行计算）
+  var callbackVisibleFooterScales = (params && params.callbackVisibleFooterScales) ? params.callbackVisibleFooterScales : function () { return 0 }
   // 窗口滚动范围Y值
   var scrollY = document.documentElement.scrollTop || document.body.scrollTop
   // 窗口高度
@@ -37,10 +39,11 @@ function onScroll (childrens, childrenRects, params) {
   // 便利子元素范围
   childrenRects.forEach(function (rect, index) {
     // 显示多少比例算可见
-    var visibleScale = callbackVisibleScales(index, rect)
+    var visibleHeaderScale = callbackVisibleHeaderScales(index, rect)
+    var visibleFooterScale = callbackVisibleFooterScales(index, rect)
     // 计算可见比例
-    var rectY = rect.y + (visibleScale > 1 ? visibleScale : rect.height * visibleScale)
-    var rectMaxY = rect.maxY
+    var rectY = rect.y + (visibleHeaderScale > 1 ? visibleHeaderScale : rect.height * visibleHeaderScale)
+    var rectMaxY = rect.maxY - (visibleFooterScale > 1 ? visibleHeaderScale : rect.height * visibleFooterScale)
     // 判断是否在显示范围内
     var isVisible = (rectY >= scrollY && rectY <= scrollMaxY) || (rectMaxY >= scrollY && rectMaxY <= scrollMaxY) || (scrollY >= rectY && scrollMaxY <= rectMaxY) || (scrollMaxY >= rectY && scrollMaxY <= rectMaxY)
     // 可见子元素
